ea4fb9c17cb327a54a91b1c13a9025fe64612837
AndreMaldo <andresxmaldonado@gmail.com>
2024-02-03 23:52:08 -0600

    Se agrega controlador de las rutas al proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view("home");
-}) -> name ("home");
-Route::get('blog', function () {
-
-    $posts = [
-        ["id" => 1, "title" => "PHP", "slug" => "php"],
-        ["id" => 2, "title" => "Laravel", "slug" => "laravel"],
-    ];
-    return view("blog",["posts" => $posts]);
-})-> name ("blog");
-
-
-
-Route::get('blog/{slug}', function ($slug) {
-    $post = $slug;
-    return view("post", ["post" => $post]);
-})-> name ("post");
-
-
+Route::controller(PageController::class)-> group(function(){
+    Route::get('/',           'home')-> name ("home");
+    Route::get('blog',        'blog')-> name ("blog");
+    Route::get('blog/{slug}', 'post')-> name ("post");
+});
