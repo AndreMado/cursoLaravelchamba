f715fb35347aeb3cdfb479d7e06a6ecdbc928490
AndreMaldo <andresxmaldonado@gmail.com>
2024-02-03 22:54:25 -0600

    Se empezó el proyecto

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("home");
+});
+Route::get('blog', function () {
+
+    $posts = [
+        ["id" => 1, "title" => "PHP", "slug" => "php"],
+        ["id" => 2, "title" => "Laravel", "slug" => "laravel"],
+    ];
+    return view("blog",["posts" => $posts] );
+});
+
+
+
+Route::get('blog/{slug}', function ($slug) {
+    $post = $slug;
+    return view("post", ["post" => $post]);
 });
 
 
